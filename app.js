c77848a05bb62bd16ae21d4d37844c168672efcb
Yao <capslock0921@gmail.com>
2020-09-03 21:32:21 +0800

    modify  register function

--- app.js
+++ app.js
@@ -46,8 +46,28 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        console.log('User a;ready exists.')
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(user => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
 })
 
 app.get('/users/logout', (req, res) => {
